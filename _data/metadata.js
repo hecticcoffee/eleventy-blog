7aff2b7380f31d1654d2ddf4fc599935f46323a4
hecticcoffee <chris@hecticcoffee.com>
2024-09-29 22:14:05 +0200

    First commit

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Hectic Coffee",
+	url: "https://hecticcoffee.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Welcome to my blog :)",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Chris",
+		email: "chris@hecticcoffee.com",
+		url: "https://hecticcoffee.com/about-me/"
 	}
 }
